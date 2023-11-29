022e886a53adc7a07a1a0b04a91553d8433821c1
adrianrtownsend <greenlight8004@gmail.com>
2023-11-29 03:48:22 -0500

    fix: updated protected routes

--- middleware.ts
+++ middleware.ts
@@ -3,5 +3,17 @@ import { withMiddlewareAuthRequired } from '@auth0/nextjs-auth0/edge'
 export default withMiddlewareAuthRequired()
 
 export const config = {
-  matcher: ['/admin', '/events', '/profile', '/protected', '/todos'],
+  matcher: [
+    '/admin',
+    '/events',
+    '/events/:path*',
+    '/posts',
+    '/posts/:path*',
+    '/profile',
+    '/protected',
+    '/todos',
+    '/todos/:path*',
+    '/users',
+    '/users/:path*',
+  ],
 }
