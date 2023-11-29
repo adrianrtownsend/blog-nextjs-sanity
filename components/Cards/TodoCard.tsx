b102e4de193c0db73fbfb6b1ea6bd6561d5ab99b
adrianrtownsend <greenlight8004@gmail.com>
2023-11-29 16:47:35 -0500

    chore: updates to todo card

--- components/Cards/TodoCard.tsx
+++ components/Cards/TodoCard.tsx
@@ -1,5 +1,8 @@
+import { ClockIcon } from '@heroicons/react/20/solid'
+import BasicSwitch from 'components/Switches/Switch'
 import { formatDistance } from 'date-fns'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export const formatDateRelative = (date: string) => {
   return formatDistance(new Date(date), new Date(), {
@@ -8,11 +11,20 @@ export const formatDateRelative = (date: string) => {
 }
 
 const TodoCard = (props) => {
-  const { date, title, user, slug } = props
+  const { date, title, user, slug, content, dueDate, completed, favorited } =
+    props
+
+  const [isCompleted, setIsCompleted] = useState(false)
+
+  const toggleCompleted = () => {
+    setIsCompleted((prev) => !prev)
+  }
+
+  console.log('todo props: ', props)
   return (
     <Link
       href={`/todos/${slug}`}
-      className="block rounded-lg p-4 shadow-sm shadow-indigo-100"
+      className="block rounded-lg p-4 shadow-sm shadow-emerald-100"
     >
       <img
         alt="Home"
@@ -22,10 +34,11 @@ const TodoCard = (props) => {
 
       <div className="mt-2">
         <dl>
-          <div>
+          <div className="flex justify-between">
             <dt className="sr-only">Title</dt>
 
             <dd className="font-medium">{title}</dd>
+            <BasicSwitch isEnabled={isCompleted} />
           </div>
           <div>
             <dt className="sr-only">User</dt>
@@ -34,25 +47,23 @@ const TodoCard = (props) => {
               {user?.name || user?.nickname}
             </dd>
           </div>
+          <div>
+            <dd className="font-medium">{content}</dd>
+          </div>
+          <div>
+            <dt className="sr-only">User</dt>
+
+            <dd className="text-sm text-gray-500">{favorited}</dd>
+          </div>
         </dl>
 
         {date && (
-          <div className="mt-6 flex items-center gap-8 text-xs">
+          <div className="mt-1 flex items-center gap-8 text-xs">
             <div className="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
-              <svg
-                className="h-4 w-4 text-indigo-700"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"
-                />
-              </svg>
+              <ClockIcon
+                className="mr-1.5 h-5 w-5 flex-shrink-0 text-green-600"
+                aria-hidden="true"
+              />
 
               <div className="mt-1.5 sm:mt-0">
                 <p className="text-gray-500">Created</p>
